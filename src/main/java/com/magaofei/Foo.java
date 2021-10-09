efbae199f974c6bc0371c52b171c3b491d7c6411
tal <mamian521@gmail.com>
2021-10-09 14:08:40 +0800

    add asm

---
+++ src/main/java/com/magaofei/Foo.java
@@ -0,0 +1,15 @@
+package com.magaofei;
+
+/**
+ * @author magaofei
+ * @date 2021/10/9
+ */
+public class Foo {
+    public void execute() {
+        System.out.println("test changed method name");
+    }
+
+    public void changeMethodContent() {
+        System.out.println("test changed method");
+    }
+}
